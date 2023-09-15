7981b93c6427a78cf54d1d9401b6d7b47799b474
madelinezhang <zhang.mad@northeastern.edu>
2023-09-15 12:30:00 -0400

    libgdx starter

---
+++ desktop/src/com/mad/capstone/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mad.capstone;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mad.capstone.MyGdxGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("capstone-game");
+		new Lwjgl3Application(new MyGdxGame(), config);
+	}
+}
