dfc55725045f10b18a0a3f60954b3ac43b8b3152
madelinezhang <zhang.mad@northeastern.edu>
2023-09-15 16:01:57 -0400

    tutorial up to platform need

--- core/src/com/mad/capstone/MyGdxGame.java
+++ core/src/com/mad/capstone/MyGdxGame.java
@@ -5,28 +5,27 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mad.capstone.screen.TitleScreen;
 
 public class MyGdxGame extends Game {
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
+
 	public SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new TitleScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/com/mad/capstone/screen/TitleScreen.java
@@ -0,0 +1,68 @@
+package com.mad.capstone.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mad.capstone.MyGdxGame;
+import com.mad.capstone.scene.Hud;
+
+public class TitleScreen implements Screen {
+  private MyGdxGame game;
+  private OrthographicCamera gameCam;
+  private Viewport gamePort;
+  private Hud hud;
+
+  public TitleScreen(MyGdxGame game) {
+    this.game = game;
+//    texture = new Texture("badlogic.jpg");
+    gameCam = new OrthographicCamera();
+    gamePort = new FitViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_HEIGHT, gameCam);
+    hud = new Hud(game.batch);
+  }
+
+  @Override
+  public void show() {
+
+  }
+
+  @Override
+  public void render(float delta) {
+    Gdx.gl.glClearColor(0, 0, 0, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+    hud.stage.draw();
+//    game.batch.begin();
+//    game.batch.draw(texture, 0, 0);
+//    game.batch.end();
+  }
+
+  @Override
+  public void resize(int width, int height) {
+    gamePort.update(width, height);
+  }
+
+  @Override
+  public void pause() {
+
+  }
+
+  @Override
+  public void resume() {
+
+  }
+
+  @Override
+  public void hide() {
+
+  }
+
+  @Override
+  public void dispose() {
+
+  }
+}
