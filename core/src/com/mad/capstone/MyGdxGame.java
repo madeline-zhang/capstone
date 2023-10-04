b7bda96eb1839d8167b77e1cda933e9cb07cacda
madelinezhang <zhang.mad@northeastern.edu>
2023-10-04 10:23:52 -0400

    working, scrolling map, but assets not committed

--- core/src/com/mad/capstone/MyGdxGame.java
+++ core/src/com/mad/capstone/MyGdxGame.java
@@ -8,8 +8,8 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.mad.capstone.screen.TitleScreen;
 
 public class MyGdxGame extends Game {
-	public static final int V_WIDTH = 400;
-	public static final int V_HEIGHT = 208;
+	public static final int V_WIDTH = 480;
+	public static final int V_HEIGHT = 320;
 
 	public SpriteBatch batch;
 	
--- core/src/com/mad/capstone/screen/TitleScreen.java
+++ core/src/com/mad/capstone/screen/TitleScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mad.capstone.MyGdxGame;
@@ -16,12 +19,27 @@ public class TitleScreen implements Screen {
   private Viewport gamePort;
   private Hud hud;
 
+  // Tiled map
+  private TmxMapLoader mapLoader;
+  private TiledMap map;
+  private OrthogonalTiledMapRenderer renderer;
+
+  // Box2d
+//  private Box2DDebugRenderer b2dRenderer;
+
   public TitleScreen(MyGdxGame game) {
     this.game = game;
 //    texture = new Texture("badlogic.jpg");
     gameCam = new OrthographicCamera();
     gamePort = new FitViewport(MyGdxGame.V_WIDTH, MyGdxGame.V_HEIGHT, gameCam);
     hud = new Hud(game.batch);
+
+    mapLoader = new TmxMapLoader();
+    map = mapLoader.load("test map/test map.tmx");
+    renderer = new OrthogonalTiledMapRenderer(map);
+    float halfWorldWidth = gamePort.getWorldWidth()/2;
+    float halfWorldHeight = gamePort.getWorldHeight()/2;
+    gameCam.position.set(halfWorldWidth, halfWorldHeight, 0);
   }
 
   @Override
@@ -29,10 +47,24 @@ public class TitleScreen implements Screen {
 
   }
 
+  private void handleInput(float dt) {
+    if(Gdx.input.isTouched()) {
+      gameCam.position.x += 100 * dt;
+    }
+  }
+
+  public void update(float dt) {
+    handleInput(dt);
+    gameCam.update();
+    renderer.setView(gameCam);
+  }
+
   @Override
-  public void render(float delta) {
+  public void render(float dt) {
+    update(dt);
     Gdx.gl.glClearColor(0, 0, 0, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    renderer.render();
 
     game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
     hud.stage.draw();
